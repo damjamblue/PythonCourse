408e72ad1b2fb88af971efdabca96eaedd508e24
Damjan Topalov <damjan.topalov@gmail.com>
2024-10-29 20:32:51 +0100

    PythonCas 5

---
+++ PythonCas5/Strings.py
@@ -0,0 +1,20 @@
+omilena_pesna = ("Last Christmas")
+
+print(omilena_pesna)
+
+print(omilena_pesna[5])
+
+print(omilena_pesna[8])
+
+recenica = "Јас учам Python"
+print(recenica[4:8])
+
+print(omilena_pesna[5:14])
+
+ime ="Damjan Topalov"
+print(ime[0])
+print(ime[7])
+
+print(ime[0:7])
+
+print(ime[7:14])
---
+++ PythonCas5/Vlez.py
@@ -0,0 +1,39 @@
+# Vlez = input("Vnesete ja vasata omilena boja")
+#
+# print(Vlez)
+#
+# CelBroj = int(input("Vnesete cel broj"))
+# DecimalenBroj = float(input("Vnesete cel broj"))
+
+#
+# vnesibroj = int(input("Vnesete cel broj:"))
+# print(vnesibroj+10)
+#
+# vnesibroj = float(input("Vnesete cel broj:"))
+# print(vnesibroj+10)
+
+# grad = "Glavniot grad na Republika Makedonija e Skopje"
+# print(grad)
+# print(grad[40])
+# print(grad[40:47])
+#
+# print("Vnesete gi stranite na triagolnikot")
+#
+# stranaA = int(input("Vnesi ja stranata A"))
+#
+# stranaB = int(input("Vnesi ja stranata B"))
+#
+# stranaC = int(input("Vnesi ja stranata C"))
+#
+# print("Perimetar:",stranaA+stranaB+stranaC)
+
+ime = input("Kako se vikas?")
+hobi = input("Koja e tvojata omilena hobi?")
+film = input("Koj e tvojot omilen film?")
+zivotno = input("Koe e tvoeto omileno zivotno?")
+oddelenie = int(input("Koe oddelenie si na ucilishte?"))
+
+print("Imeto na korisnikot e ", ime, "i toj e ucenik vo ", oddelenie,".Negovoto omileno zhivotno e", zivotno,"Omileno hobi", hobi, "a omilena kniga film e", film)
+
+
+
