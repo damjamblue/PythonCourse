bed47e0989472cbf523597a075841763981b65bb
Damjan Topalov <damjan.topalov@gmail.com>
2024-10-29 12:34:39 +0100

    Dodadovme koristenje na right ako imame negativen agol za left

---
+++ Cas3/Cas3zad6.py
@@ -0,0 +1,40 @@
+from turtle import *
+
+a = 80
+b = 40
+angle = 90
+d = 40
+angle2 = 180
+angle3 = 30
+angle4 = 60
+angle5 = 120
+angle6 = 30
+doPozicijaNaVrata = 20
+dimenzijaNaVrata = 40
+
+
+forward(a)
+left(angle)
+forward(a)
+left(angle)
+forward(a)
+left(angle)
+forward(a)
+left(angle2)
+forward(a)
+right(angle3)
+forward(a)
+right(angle5)
+forward(a)
+right(angle3)
+forward(a)
+right(angle)
+forward(doPozicijaNaVrata)
+right(angle)
+forward(dimenzijaNaVrata)
+left(angle)
+forward(dimenzijaNaVrata)
+left(angle)
+forward(dimenzijaNaVrata)
+
+done()
