c5118fde5a2676771e49f1becd70ee8a251037c6
Damjan Topalov <damjan.topalov@gmail.com>
2024-11-19 20:32:01 +0100

    PythonCas8

---
+++ PythonCas8/listi.py
@@ -0,0 +1,31 @@
+# supermarket_list = ['mandarini','limoni','chokolado','leb','chaj od nane']
+# print(supermarket_list[0]);
+#
+# supermarket_list.append('domati')
+# supermarket_list.append('krusi')
+#
+# print(supermarket_list)
+#
+# supermarket_list.remove('domati')
+#
+# print(supermarket_list)
+#
+# supermarket_list.remove(supermarket_list[1])
+# print(supermarket_list)
+
+list = [5, 20, 15, 20, 25, 50, 20]
+print(list)
+list.remove(list[1])
+print(list)
+list.remove(list[5])
+print(list)
+list.remove(list[4])
+print(list)
+
+
+lista = [4,7,13,16,18]
+
+print("Zbirot na site vrednosti od listata e:",lista[0]+lista[1]+lista[2]+lista[3]+lista[4])
+listanova = [lista[0]*5,lista[1]*5,lista[2]*5,lista[3]*5,lista[4]*5]
+print(listanova)
+
---
+++ PythonCas8/predmeti.py
@@ -0,0 +1,4 @@
+predmeti = ['matematika','makedonski','tehnicko','prirodni','istorija I opshtestvo','angliski','fizichko','muzichko']
+print(predmeti);
+print("Mojot omilen predmet e:",predmeti[5])
+
