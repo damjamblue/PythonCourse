d80760ebd8bfe3cc09753c38e2bb740f9abf33e0
Damjan Topalov <damjan.topalov@gmail.com>
2024-11-19 19:00:21 +0100

    Domashno Chas 6

---
+++ PythonCas6/DomashnoCas6.py
@@ -0,0 +1,23 @@
+prashanje1 = input("Koja e tretata planeta od nashiot soncev sistem?")
+prashanje2 = input("Kolku strani ima mnoguagolnikot?")
+prashanje3 = input("Na kolku stepeni vrie vodata?")
+
+tochniOdgovori= 0
+
+if prashanje1.lower() == "treta" or 3:
+    print("Tocen e odgovorot")
+    tochniOdgovori=tochniOdgovori+1
+else:
+    print("Odgovorot ne e tocen")
+if prashanje2.lower() == "mnogu":
+    print("Tocen e odgovorot")
+    tochniOdgovori=tochniOdgovori+1
+else:
+    print("Odgovorot ne e tocen")
+if prashanje3.lower() == "100":
+    print("Odgovorot e tocen")
+    tochniOdgovori=tochniOdgovori+1
+else:
+    print("Odgovorot ne e tocen")
+
+print("Imate tochni odgovori:", tochniOdgovori)
