a261881b340cf8b48f60618461c0943264abab97
Damjan Topalov <damjan.topalov@gmail.com>
2025-02-04 17:17:22 +0100

    Domashno Chas 14

--- TrkasoZelki/Domashno_TrkaSoZhelki.py
+++ TrkasoZelki/Domashno_TrkaSoZhelki.py
@@ -1,53 +1,46 @@
-import turtle
 from turtle import *
-
 import random
 
-# Prva zelka
+# Prva zhelka
 prv_igrach = Turtle()
 prv_igrach.shape('turtle')
 prv_igrach.color('green')
 prv_igrach.penup()
-prv_igrach.goto(-200,100)
+prv_igrach.goto(-100, 200)
 # Prva cel
-
-prv_igrach.goto(300,60)
+prv_igrach.goto(300, 160)
 prv_igrach.pendown()
 prv_igrach.circle(40)
 prv_igrach.penup()
-prv_igrach.goto(-100,100)
+prv_igrach.goto(-100, 200)
 
-# Vtora zelka
+# Vtora zhelka
 vtor_igrach = Turtle()
 vtor_igrach.shape('turtle')
 vtor_igrach.color('blue')
 vtor_igrach.penup()
-vtor_igrach.goto(-200, -100)
-
+vtor_igrach.goto(-200, -200)
 # Vtora cel
-vtor_igrach.goto(300, -140)
+vtor_igrach.goto(300, -240)
 vtor_igrach.pendown()
 vtor_igrach.circle(40)
 vtor_igrach.penup()
-vtor_igrach.goto(-200,-100)
-
+vtor_igrach.goto(-200, -200)
 
 kocka = [1,2,3,4,5,6]
-
-#Nedopishano
 
 while True:
     frlena_kocka = random.randint(kocka[0], kocka[len(kocka) - 1])
     prv_igrach.forward(frlena_kocka)
 
-    frlena_kocka2 = random.randint(kocka[0], kocka[len(kocka) - 1])
-    vtor_igrach.forward(frlena_kocka2)
+    frlena_kocka = random.randint(kocka[0], kocka[len(kocka) - 1])
+    vtor_igrach.forward(frlena_kocka)
 
     if (prv_igrach.xcor() >= 260):
-        turtle.write('Prviot igrach e pobednik')
+        print("Prviot igrach e pobednik")
         break
     if (vtor_igrach.xcor() >= 260):
-        turtle.write('Vtoriot igrach e pobednik')
+        print("Vtoriot igrach e pobednik")
         break
 
 done()
